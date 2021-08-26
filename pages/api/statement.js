8b86d7958255715cbb716ebb3d1ae7e42653fa6b
naurinah <87181806+naurinah@users.noreply.github.com>
2021-08-26 03:32:49 -0700

    Update statement.js

--- pages/api/statement.js
+++ pages/api/statement.js
@@ -5,10 +5,10 @@ export default statement(async (req, res) => {
     const [cookie, setCookie] = useCookies(["user"])
     console.log(cookie.user)
     const { username } = await req.body;
-//     const url = `http://benefitx.blue-ex.com/api/customerportal/statement.php?acno=$${acno}`;
+    const url = `http://benefitx.blue-ex.com/api/customerportal/statement.php?acno=$${acno}`;
   
-//     fetch(baseURL)
-//         .then(response => response.json())
-//         .then(data => console.log(data)
-//         );
+    fetch(baseURL)
+        .then(response => response.json())
+        .then(data => console.log(data)
+        );
   });
